145e1036184809ee98df23a330c4e732c0421ede
FoxyBT35 <twrmlys@gmail.com>
2026-04-23 22:18:55 +0800

    Update TaskTest.java

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,138 +1,88 @@
 import IP.TodoListApplication.App.Task;
-import IP.TodoListApplication.DataSorting.DateSorting;
 import org.junit.Test;
 
 import java.time.LocalDate;
 
 import static org.junit.Assert.*;
 
-
 public class TaskTest {
 
     @Test
     public void getId() {
         Task task = new Task();
-
         task.setId("1");
-
-        String expected = "1";
-        assertEquals(expected, task.getId());
-
+        assertEquals("1", task.getId());
     }
 
     @Test
     public void getTitle() {
         Task task = new Task();
-
-        task.setId("1");
-
-        String expected = "1";
-        assertEquals(expected, task.getId());
+        task.setTitle("Test");
+        assertEquals("Test", task.getTitle());
     }
 
     @Test
     public void getDueDate() {
-
-
         Task task = new Task();
-
         LocalDate dueDate = LocalDate.of(2020, 12, 12);
         task.setDueDate(dueDate);
-
-        LocalDate expected = LocalDate.of(2020, 12, 12);
-
-        assertEquals(expected, task.getDueDate());
-
+        assertEquals(dueDate, task.getDueDate());
     }
-
 
     @Test
     public void getStatus() {
         Task task = new Task();
-
-        task.setTitle("Working");
-
-        String expected = "Working";
-        assertEquals(expected, task.getTitle());
-
+        task.setStatus("Working");
+        assertEquals("Working", task.getStatus());
     }
 
     @Test
     public void getProjectName() {
         Task task = new Task();
-
         task.setProjectName("Coding");
-
-        String expected = "Coding";
-        assertEquals(expected, task.getProjectName());
-
+        assertEquals("Coding", task.getProjectName());
     }
 
     @Test
     public void setId() {
         Task task = new Task();
-
         task.setId("1122");
-
-        String expected = "1122";
-        assertEquals(expected, task.getId());
+        assertEquals("1122", task.getId());
     }
 
     @Test
     public void setTitle() {
         Task task = new Task();
-
         task.setTitle("Clear-Cache");
-
-        String expected = "Clear-Cache";
-        assertEquals(expected, task.getTitle());
-
+        assertEquals("Clear-Cache", task.getTitle());
     }
 
     @Test
     public void setDueDate() {
-
-
         Task task = new Task();
-
         LocalDate dueDate = LocalDate.of(2021, 10, 13);
-
         task.setDueDate(dueDate);
-
-        LocalDate expected = LocalDate.of(2021, 10, 13);
-
-        assertEquals(expected, task.getDueDate());
-
+        assertEquals(dueDate, task.getDueDate());
     }
 
     @Test
     public void setStatus() {
         Task task = new Task();
-
         task.setStatus("in-progress");
-
-        String expected = "in-progress";
-        assertEquals(expected, task.getStatus());
-
+        assertEquals("in-progress", task.getStatus());
     }
 
     @Test
     public void setProjectName() {
         Task task = new Task();
-
         task.setProjectName("Monthly-Supply");
-
-        String expected = "Monthly-Supply";
-        assertEquals(expected, task.getProjectName());
+        assertEquals("Monthly-Supply", task.getProjectName());
     }
 
     @Test
     public void setFavorite() {
         Task task = new Task();
-
         task.setFavorite(true);
-
         assertTrue(task.isFavorite());
-}
-
+    }
 }
